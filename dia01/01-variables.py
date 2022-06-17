485f0b4229304243faeb4aafcc3e899ae108342a
Andrew Medina <henry.medina@tecsup.edu.pe>
2022-06-16 21:02:23 -0500

    commit de operadores , etc

---
+++ dia01/01-variables.py
@@ -0,0 +1,16 @@
+# commentaries
+nombre='Name001'
+edad=10
+saludo="aloh'a coño"
+
+despedida='''bueno
+Hoola coño
+'''
+
+fecha_de_definicion=None #Nula==undefined in JS
+# tabulaciones o identaciones
+variable_1,variable_2,variable_03="hola","donde","estais"
+
+hola="hola"
+print("""
+{}""".format(hola)*5)
